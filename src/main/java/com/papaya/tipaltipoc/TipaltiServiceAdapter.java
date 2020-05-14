444595f47be796761dd8d7c6f5c08c9d85f429c1
gennadyzaytsev <gennadyza@papayaglobal.com>
2020-05-14 13:12:33 +0300

    plugin fix

--- src/main/java/com/papaya/tipaltipoc/TipaltiServiceAdapter.java
+++ src/main/java/com/papaya/tipaltipoc/TipaltiServiceAdapter.java
@@ -4,8 +4,9 @@ import javax.xml.bind.JAXBElement;
 
 public class TipaltiServiceAdapter {
 
-    public GetBankResponseType getBank(String url, Object request){
+  /*  public GetBankResponseType getBank(String url, Object request) {
         JAXBElement res = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(url, request);
         return res.getValue();
 
-    }
+    }*/
+}
